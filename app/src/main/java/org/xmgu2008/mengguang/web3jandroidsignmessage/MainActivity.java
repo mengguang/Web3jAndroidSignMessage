f41e79e91918bfee9f98f787655bdaf0ce185124
Meng Guang <mengguang@newtonproject.org>
2018-08-20 19:33:56 +0800

    add verify method.

--- app/src/main/java/org/xmgu2008/mengguang/web3jandroidsignmessage/MainActivity.java
+++ app/src/main/java/org/xmgu2008/mengguang/web3jandroidsignmessage/MainActivity.java
@@ -9,6 +9,13 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
+import org.spongycastle.asn1.x9.X9ECParameters;
+import org.spongycastle.crypto.ec.CustomNamedCurves;
+import org.spongycastle.crypto.params.ECDomainParameters;
+import org.spongycastle.crypto.params.ECPublicKeyParameters;
+import org.spongycastle.crypto.signers.ECDSASigner;
+import org.spongycastle.math.ec.ECPoint;
+import org.spongycastle.math.ec.custom.sec.SecP256R1Curve;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.ECDSASignature;
 import org.web3j.crypto.Hash;
@@ -47,11 +54,11 @@ public class MainActivity extends Activity {
                 return false;
             }
         };
-        if(cwd.listFiles(filter).length == 0) {
+        if (cwd.listFiles(filter).length == 0) {
             try {
                 fileName = WalletUtils.generateNewWalletFile(
-                        "123qwe",
-                        cwd,false);
+                        Password,
+                        cwd, false);
                 System.out.println(fileName);
                 writeLogMessage(fileName);
             } catch (Exception e) {
@@ -86,14 +93,30 @@ public class MainActivity extends Activity {
         System.out.println(hexHash);
 
         ECDSASignature signature = credentials.getEcKeyPair().sign(hash);
-
-        String r = Numeric.toHexStringNoPrefixZeroPadded(signature.r,64);
-        String s = Numeric.toHexStringNoPrefixZeroPadded(signature.s,64);
-
+        String r = Numeric.toHexStringNoPrefixZeroPadded(signature.r, 64);
+        String s = Numeric.toHexStringNoPrefixZeroPadded(signature.s, 64);
         String hexSignature = r + s;
-
         System.out.println(hexSignature);
 
+        boolean result = verifyData(hexPublicKey, hash, signature);
+        System.out.println("verify result: " + result);
+    }
+
+    private boolean verifyData(String hexPublicKey, byte[] message, ECDSASignature signature) {
+        ECDSASigner signer = new ECDSASigner();
+
+        X9ECParameters CURVE_PARAMS = CustomNamedCurves.getByName("secp256r1");
+        ECDomainParameters CURVE = new ECDomainParameters(
+                CURVE_PARAMS.getCurve(), CURVE_PARAMS.getG(), CURVE_PARAMS.getN(), CURVE_PARAMS.getH());
+
+        SecP256R1Curve s = new SecP256R1Curve();
+        ECPoint p = s.decodePoint(Numeric.hexStringToByteArray(hexPublicKey));
+
+        ECPublicKeyParameters publicKeyParm = new ECPublicKeyParameters(p, CURVE);
+
+        signer.init(false, publicKeyParm);
+
+        return signer.verifySignature(message, signature.r, signature.s);
     }
 
     @UiThread
